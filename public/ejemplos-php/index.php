d9dfdc28704f2ad99bffb6f1d5c9b07ee61a685a
Marce Concepción <marcelinocb@gmail.com>
2022-08-09 20:12:11 +0100

    organizados ficheros

---
+++ public/ejemplos-php/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejemplos Lenguaje PHP</title>
+</head>
+<body>
+    <h1>Ejemplos Lenguaje PHP</h1>
+    <ul>
+        <li><a href="./fizzbuzz.php">FizzBuzz</a></li>
+        <li><a href="./funciones.php">Funciones</a></li>
+        <li><a href="./formularios/numero.php">Ejemplo formulario GET</a></li>
+        <li><a href="./formularios/registro.php">Ejemplo formulario POST</a></li>
+        <li><a href="./formularios/saludar.php">Formulario y manejador en el mismo archivo</a></li>
+        <li><a href="./ejemplo/index.php">Ejemplo conexión a Base de Datos</a></li>
+    </ul>
+</body>
+</html>
